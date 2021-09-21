92dc38e199742c5af35a14633605ff7f73fa7656
Davi-Arauj <moreiradaavi@gmail.com>
2021-09-21 19:18:23 -0300

    modification in product.service

--- src/main/java/api/restaurant/product/service/ProductService.java
+++ src/main/java/api/restaurant/product/service/ProductService.java
@@ -6,11 +6,9 @@ import org.springframework.stereotype.Service;
 import api.restaurant.product.dto.ProductDTO;
 import api.restaurant.product.dto.response.ProductResponseDTO;
 import api.restaurant.product.entity.Product;
-import api.restaurant.product.mapper.ProductMap;
 import api.restaurant.product.mapper.ProductMapImp;
 import api.restaurant.product.repository.ProductRepository;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
