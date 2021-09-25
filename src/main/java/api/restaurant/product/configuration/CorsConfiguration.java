9f20515c35229d6ccbba76c9f33bedd9f6ec6a98
Davi-Arauj <moreiradaavi@gmail.com>
2021-09-25 18:06:38 -0300

    Configuration CORS ++

--- src/main/java/api/restaurant/product/configuration/CorsConfiguration.java
+++ src/main/java/api/restaurant/product/configuration/CorsConfiguration.java
@@ -1,8 +1,10 @@
 package api.restaurant.product.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer{
 
 	@Override
