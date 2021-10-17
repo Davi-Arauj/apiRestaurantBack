eabbdf7a213efdf74007c0f2e132238bb0b32f0f
Davi-Arauj <moreiradaavi@gmail.com>
2021-10-16 21:53:19 -0300

    Validation email and Update

---
+++ src/main/java/api/restaurant/service/validation/ClientUpdateValidator.java
@@ -0,0 +1,52 @@
+package api.restaurant.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import api.restaurant.dto.ClientDTO;
+import api.restaurant.entity.Client;
+import api.restaurant.exception.FieldMessage;
+import api.restaurant.repository.ClientRepository;
+
+public class ClientUpdateValidator implements ConstraintValidator<ClientUpdate, ClientDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClientRepository repo;
+	
+	@Override
+	public void initialize(ClientUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClientDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Client aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFiledName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
