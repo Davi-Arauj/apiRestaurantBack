76804e6d864c1de2856c9be6bcc4932ca60c547c
Davi-Arauj <moreiradaavi@gmail.com>
2021-12-02 21:20:02 -0300

    Saving first file to S3

--- src/main/java/api/restaurant/RestaurantApplication.java
+++ src/main/java/api/restaurant/RestaurantApplication.java
@@ -1,52 +1,28 @@
 package api.restaurant;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import api.restaurant.dto.ProductDTO;
-import api.restaurant.entity.Address;
-import api.restaurant.entity.Category;
-import api.restaurant.entity.City;
-import api.restaurant.entity.Client;
-import api.restaurant.entity.OrderedItem;
-import api.restaurant.entity.Payment;
-import api.restaurant.entity.PaymentWithBoleto;
-import api.restaurant.entity.PaymentWithCard;
-import api.restaurant.entity.Pedido;
-import api.restaurant.entity.Product;
-import api.restaurant.entity.State;
-import api.restaurant.entity.enums.Paymentstate;
-import api.restaurant.entity.enums.TypeClient;
-import api.restaurant.mapper.ProductMaping;
-import api.restaurant.repository.AddressRepository;
-import api.restaurant.repository.CategoryRepository;
-import api.restaurant.repository.CityRepository;
-import api.restaurant.repository.ClientRepository;
-import api.restaurant.repository.OrderedItemRepository;
-import api.restaurant.repository.PaymentRepository;
-import api.restaurant.repository.PedidoRepository;
-import api.restaurant.repository.ProductRepository;
-import api.restaurant.repository.StateRepository;
+import api.restaurant.service.S3Service;
 
 @SpringBootApplication
 public class RestaurantApplication implements CommandLineRunner {
+	
+	
+	@Autowired
+	private S3Service s3Service;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApplication.class, args);
 
-	}
+	} 
 	
-	
-	
-
 	@Override
-	public void run(String... args) throws Exception {
-		
-	}
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("/home/d/Documents/workspace-spring-tool-suite-4-4.11.0.RELEASE/restaurant/ft-jayane-rosto.jpeg");
+	}	
 
 }
