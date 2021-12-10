0a012c9f2d5b20c4ebeb6142bda6842272b608e1
Davi-Arauj <moreiradaavi@gmail.com>
2021-12-10 00:05:17 -0300

    Standardizing the format of exceptions

--- src/main/java/api/restaurant/exception/ValidationError.java
+++ src/main/java/api/restaurant/exception/ValidationError.java
@@ -8,18 +8,20 @@ import api.restaurant.service.exception.StandarError;
 public class ValidationError extends StandarError {
 	private static final long serialVersionUID = 1L;
 	
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
+	
+	
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
 		
 	}
-	
+
 	private List<FieldMessage> errors = new ArrayList<>();
 	
 	public List<FieldMessage> getErrors(){
 		return errors;
 	}
 	
-	public void addErrors(String fieldName, String msg) {
+	public void addError(String fieldName, String msg) {
 		errors.add(new FieldMessage(fieldName,msg));
 	}
 	
--- src/main/java/api/restaurant/service/exception/StandarError.java
+++ src/main/java/api/restaurant/service/exception/StandarError.java
@@ -2,20 +2,20 @@ package api.restaurant.service.exception;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class StandarError implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	private Integer status;
-	private String msg;
-	private Long timeStamp;
 	
-	public StandarError(Integer status, String msg, Long timeStamp) {
-		super();
-		this.status = status;
-		this.msg = msg;
-		this.timeStamp = timeStamp;
-	}
+	private Long timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+	
+	
 }
